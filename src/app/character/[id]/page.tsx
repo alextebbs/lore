7557f962035156817d0e0a3430cef3040d0e9034
Alex Tebbs <alex@alextebbs.com>
2023-06-02 22:12:53 -0700

    ???

--- src/app/character/[id]/page.tsx
+++ src/app/character/[id]/page.tsx
@@ -1,33 +1,5 @@
-import { PrismaClient } from "@prisma/client/edge";
-import { CharacterSheet } from "~/components/CharacterSheet";
-import { type Character } from "@prisma/client";
-
-export default async function Page({ params }: { params: { id: string } }) {
+export default function Page({ params }: { params: { id: string } }) {
   const { id } = params;
 
-  return <div>Character {params.id} not found.</div>;
-
-  const prisma = new PrismaClient();
-
-  const getCharacter = async (id: string) => {
-    try {
-      const character: Character | null = await prisma.character.findUnique({
-        where: { id },
-      });
-
-      return character;
-    } catch (error: unknown) {
-      console.error(error);
-      return null;
-    }
-  };
-
-  const character: Character | null = await getCharacter(id);
-
-  if (!character) {
-    // Render an error message or redirect if character is null
-    return <div>Character {params.id} not found.</div>;
-  }
-
-  return <CharacterSheet character={character} />;
+  return <div>Character {id} not found.</div>;
 }
