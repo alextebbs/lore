989408eae78369775b7957aa82d0459e7438688a
Alex Tebbs <alex@alextebbs.com>
2023-06-07 15:44:11 -0700

    Move /character page back to page router because Vercel is being dumb

---
+++ src/pages/character/[id].tsx
@@ -0,0 +1,43 @@
+import { PrismaClient } from "@prisma/client/edge";
+import { type Character } from "@prisma/client";
+import { type GetServerSideProps } from "next";
+import { CharacterSheet } from "~/components/CharacterSheet";
+
+import "~/styles/globals.css";
+
+interface PageProps {
+  character: Character | null;
+}
+
+export default function Page(props: PageProps) {
+  const { character } = props;
+
+  if (!character) {
+    // Render an error message or redirect if character is null
+    return <div>Character not found.</div>;
+  }
+
+  return <CharacterSheet character={character} />;
+}
+
+export const getServerSideProps: GetServerSideProps<PageProps> = async (
+  context
+  // eslint-disable-next-line @typescript-eslint/require-await
+) => {
+  const { id } = context.query;
+
+  const prisma = new PrismaClient();
+
+  try {
+    const character = await prisma.character.findUnique({
+      where: { id: id as string },
+    });
+
+    return {
+      props: { character: JSON.parse(JSON.stringify(character)) as Character },
+    };
+  } catch (error: unknown) {
+    console.error(error);
+    return { props: { character: null } };
+  }
+};
