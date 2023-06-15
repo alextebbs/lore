4925091f8955ddb0f6eca28bac40d066c0124ce5
Alex Tebbs <alex@alextebbs.com>
2023-06-14 23:16:11 -0700

    Remove header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,26 +11,5 @@ export const Header: React.FC<HeaderProps> = (props) => {
 
   const { data: session } = useSession();
 
-  return (
-    <div className="flex items-center justify-center border-b border-b-stone-900 px-8 py-4 text-center uppercase text-stone-400">
-      <div className="font-heading text-3xl">Mythweaver</div>
-      <div className="ml-auto text-sm text-stone-400">
-        {session ? (
-          <>
-            {/* <div onClick={(_) => setMenuOpen((prev) => !prev)}>Show Menu</div> */}
-          </>
-        ) : (
-          <>
-            <button
-              onClick={() => signIn()}
-              className="uppercase text-stone-300 hover:text-red-600"
-            >
-              Sign in
-            </button>{" "}
-            to save your stuff
-          </>
-        )}
-      </div>
-    </div>
-  );
+  return <div className=""></div>;
 };
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -12,35 +12,50 @@ export const Sidebar: React.FC<SidebarProps> = (props) => {
 
   return (
     <div className="w-80 border-l border-l-stone-900">
-      {/* <div className="p-4 text-xs uppercase tracking-[0.25em] text-stone-600">
-        Your characters
-      </div> */}
-      <Link href={`/`}>
-        <div
-          className={`border-b border-b-stone-900 p-4 font-body text-sm uppercase text-red-600 hover:bg-stone-900`}
-        >
-          + New character
-        </div>
-      </Link>
-      {characters?.map((character) => (
-        <Link key={character.id} href={`/character/${character.id}`}>
-          <div
-            className={`border-b border-b-stone-900 p-4 font-heading text-2xl ${
-              currentCharacter?.id == character.id
-                ? `bg-stone-900 text-stone-100`
-                : `text-stone-400 hover:text-red-600`
-            } `}
-          >
-            {character.name || "Unnamed character"}
+      <div className="border-b border-stone-900 p-4 font-heading text-3xl uppercase">
+        Mythweaver
+      </div>
+      {characters && (
+        <>
+          <Link href={`/`}>
+            <div
+              className={`border-b border-b-stone-900 p-4 font-body text-sm uppercase text-red-600 hover:bg-stone-900`}
+            >
+              + New character
+            </div>
+          </Link>
+          {characters?.map((character) => (
+            <Link key={character.id} href={`/character/${character.id}`}>
+              <div
+                className={`border-b border-b-stone-900 p-4 font-heading text-2xl ${
+                  currentCharacter?.id == character.id
+                    ? `bg-stone-900 text-stone-100`
+                    : `text-stone-400 hover:text-red-600`
+                } `}
+              >
+                {character.name || "Unnamed character"}
 
-            {character.age && character.species && (
-              <div className="font-body text-sm text-stone-600">
-                {character.age} year old {character.species}
+                {character.age && character.species && (
+                  <div className="font-body text-sm text-stone-600">
+                    {character.age} year old {character.species}
+                  </div>
+                )}
               </div>
-            )}
-          </div>
-        </Link>
-      ))}
+            </Link>
+          ))}
+        </>
+      )}
+      {!characters && (
+        <>
+          <button
+            onClick={() => signIn()}
+            className="uppercase text-stone-300 hover:text-red-600"
+          >
+            Sign in
+          </button>{" "}
+          to save your stuff
+        </>
+      )}
     </div>
   );
 };
