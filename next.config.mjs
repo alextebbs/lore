9b4a8e646d137c8a07a75627b42f5e0e034c349f
Alex Tebbs <alex@alextebbs.com>
2023-06-18 16:54:20 -0700

    trying to fix routing

--- next.config.mjs
+++ next.config.mjs
@@ -23,9 +23,9 @@ const config = {
    *
    * @see https://github.com/vercel/next.js/issues/41980
    */
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
+  // i18n: {
+  //   locales: ["en"],
+  //   defaultLocale: "en",
+  // },
 };
 export default config;
